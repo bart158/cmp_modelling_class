633ca04ca69fb71a37fda6992eacd651786b4a19
Bartłomiej Brudnowski <bartek.brudnowski@gmail.com>
2024-03-06 00:27:07 +0100

    Finished 1D automaton

--- class_01/1Dautomaton.py
+++ class_01/1Dautomaton.py
@@ -1,22 +1,26 @@
 import numpy as np
 from PIL import Image, ImageDraw
 
-rule = 150
-rule_bin = np.binary_repr(rule)
-ruledict = {
-    7: rule_bin[0]
-    6: rule_bin[1]
-    5: rule_bin[2]
-    4: rule_bin[3]
-    3: rule_bin[4]
-    2: rule_bin[5]
-    1: rule_bin[6]
-    0: rule_bin[7]
-}
+
+rule = input("Give rule number:")
+rule_bin_str = np.binary_repr(int(rule), width=8)
+rule_bin = np.array([int(ch) for ch in rule_bin_str], dtype=np.int8)
+
 board = np.zeros((1000,101))
 
 board[0, 50] = 1
-for i in range(1, len(board)):
-    
 
-print(board[0])
+for i in range(1, len(board)):
+    leftcell = np.roll(board[i-1], 1)
+    rightcell = np.roll(board[i-1], -1)
+    sumboard = leftcell*4 + board[i-1]*2 + rightcell
+    board[i] = rule_bin[7-sumboard.astype(np.int8)]
+
+
+img = Image.new("RGB",(101, 1000),(255,255,255))
+draw = ImageDraw.Draw(img)
+for y in range(1000):
+    for x in range(101):
+        if board[y][x]:
+            draw.point((x,y),(0,0,0))
+img.save(f"ca{rule}.PNG")
