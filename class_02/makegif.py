213b86ea79e4ec2409149e279f75635ae1f443d3
Bartłomiej Brudnowski <bartek.brudnowski@gmail.com>
2024-03-06 10:59:06 +0100

    Spatial PD task 1

---
+++ class_02/makegif.py
@@ -0,0 +1,13 @@
+import imageio
+
+images = []
+filenames = []
+
+for i in range(0,100):
+    filenames.append("spatialPD_frames/frame{0:05d}.PNG".format(i))
+
+with imageio.get_writer('spatialPD_movie_b_208.gif', mode='I', duration = 0.5) as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+
