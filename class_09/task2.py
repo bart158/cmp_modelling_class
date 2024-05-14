7f581c7ab2d79d5583dbf50a515c333d7f4c3f5f
Bartłomiej Brudnowski <bartek.brudnowski@gmail.com>
2024-05-14 15:30:44 +0200

    Task 2 class 09  progress

--- class_09/task2.py
+++ class_09/task2.py
@@ -7,26 +7,28 @@ def neighbours(i, L):
     ix, iy = i
     return [(ix, (iy+1)%L), (ix, (iy-1)%L), ((ix + 1)%L, iy), ((ix - 1)%L, iy)]
 
-def update(i, s, L):
+def update(i, s, L, aval, aval_count):
     s[i] = 1
     for j in neighbours(i, L):
         h_loc[j] += 2
+        aval[j] = aval_count
     return
 
-def do_avalanche(h_loc, s, H):
+def do_avalanche(h_loc, s, H, aval, aval_count):
     d = []
     inqueue = np.zeros((L, L), dtype = int)
-    i_trig = np.unravel_index(np.argmax(h_loc + (s+1)*(-100)), h_loc.shape)
+    totrig = (h_loc+H) >= 0
+    i_trig = totrig.nonzero()
     #print("i_trig ", (i_trig))
-    H -= h_loc[i_trig]
-    d.append(i_trig)
+    for i in range(0, len(i_trig[0])):
+        d.append((i_trig[0][i], i_trig[1][i]))
 
     while len(d) > 0:
         #print("d", d)
         itemp = d.pop(0)
         #print("s[itemp] ", (s[itemp]))
         if s[itemp] == -1:
-            update(itemp, s, L)
+            update(itemp, s, L, aval, aval_count)
             itoflip = np.transpose((np.logical_and(np.logical_and((h_loc + H) >= 0, s < 0), inqueue == 0)).nonzero())
             for i in itoflip:
                 d.append((i[0], i[1]))
@@ -36,47 +38,30 @@ def do_avalanche(h_loc, s, H):
     
     return np.sum(s)
 
-L = 100
-R = 0.7
-H = 0
+L = 300
+R = 2.1
+H = -3
 h_rnd = np.random.randn(L, L) * R
 h_loc = np.ones((L, L)) * -4 + h_rnd
 s = np.ones((L, L), dtype=int) * -1
+aval = np.zeros((L, L), dtype=int)
+aval_count = 0
 
-avg1 = 0
-for i in range(0, 1000):
-    R = 0.7
-    H = 0
-    h_rnd = np.random.randn(L, L) * R
-    h_loc = np.ones((L, L)) * -4 + h_rnd
-    s = np.ones((L, L), dtype=int) * -1
-    avg1 += do_avalanche(h_loc, s, H)/1000
-    #print("s {}", (s))
-    if i%10 == 0:
-        print(i)
+hval = [H]
+mval = [np.sum(s)/(L*L)]
 
-avg2 = 0
-for i in range(0, 1000):
-    R = 0.9
-    H = 0
-    h_rnd = np.random.randn(L, L) * R
-    h_loc = np.ones((L, L)) * -4 + h_rnd
-    s = np.ones((L, L), dtype=int) * -1
-    avg2 += do_avalanche(h_loc, s, H)/1000
-    if i%100 == 0:
-        print(i)
+while H <= 3:
+    aval_count += 1
+    do_avalanche(h_loc, s, H, aval, aval_count)
+    hval.append(H)
+    mval.append(np.sum(s)/(L*L))
+    H += 0.1
 
-avg3 = 0
-for i in range(0, 1000):
-    R = 1.4
-    H = 0
-    h_rnd = np.random.randn(L, L) * R
-    h_loc = np.ones((L, L)) * -4 + h_rnd
-    s = np.ones((L, L), dtype=int) * -1
-    avg3 += do_avalanche(h_loc, s, H)/1000
-    if i%100 == 0:
-        print(i)
 
-print( "R = {} \t mean_size = {}".format(0.7, (avg1 + L*L)/2))
-print( "R = {} \t mean_size = {}".format(0.9, (avg2 + L*L)/2))
-print( "R = {} \t mean_size = {}".format(1.4, (avg3 + L*L)/2))
+plt.imshow(aval,interpolation='none',cmap="gist_rainbow")
+plt.savefig("task2a.png")
+plt.clf()
+plt.plot(mval, hval)
+plt.xlabel("M")
+plt.ylabel("H")
+plt.savefig("histeresis_r21.png")
