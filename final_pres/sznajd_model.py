1610d9155fd86a89189d7b79b263f4e82093e677
Bartłomiej Brudnowski <bartek.brudnowski@gmail.com>
2024-06-10 00:59:08 +0200

    Fixed MC step

--- final_pres/sznajd_model.py
+++ final_pres/sznajd_model.py
@@ -2,17 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def MCstep(chain):
-    i = np.random.randint(0, len(chain)-1)
-    if chain[i] == chain[i+1]:
-        if i-1 >= 0:
-            chain[i-1] = chain[i]
-        if i+2 < len(chain):
-            chain[i+2] = chain[i]
-    else:
-        if i-1 >= 0:
-            chain[i-1] = chain[i+1]
-        if i+2 < len(chain):
-            chain[i+2] = chain[i]
+    for j in range(0, len(chain)):
+        i = np.random.randint(0, len(chain)-1)
+        if chain[i] == chain[i+1]:
+            if i-1 >= 0:
+                chain[i-1] = chain[i]
+            if i+2 < len(chain):
+                chain[i+2] = chain[i]
+        else:
+            if i-1 >= 0:
+                chain[i-1] = chain[i+1]
+            if i+2 < len(chain):
+                chain[i+2] = chain[i]
     return chain
 
 def getMag(chain):
@@ -25,7 +26,7 @@ def check_if_changed(chain, chain_copy, time_arr, n_of_step):
             time_arr[i].append(n_of_step)
     #print(isChanged)
 
-N = 100
+N = 1000
 
 chain = np.random.randint(0,2,(N))*2 - 1
 edges = np.linspace(0, N, N+1, endpoint=True)
@@ -34,7 +35,7 @@ m = []
 time_arr_numpy = np.zeros((N, 1), dtype= int)
 time_arr = time_arr_numpy.tolist()
 
-for i in range(0, 10000):
+for i in range(0, 1000):
     x.append(i)
     chain_copy = np.copy(chain)
     chain = MCstep(chain)
@@ -52,7 +53,11 @@ for iTime in time_arr:
         tau.append(iTime[i+1] - iTime[i])
 
 plt.clf()
-plt.hist(tau)
+n, bins, patches = plt.hist(tau, 50)
+plt.cla()
+taux = bins + (bins[1]-bins[0])/2
+taux = taux[0:-1]
+plt.scatter(taux, n)
 plt.xscale('log')
 plt.yscale('log')
 plt.savefig('tauhist.png')
